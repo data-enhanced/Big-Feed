a3ca1925a6ccda937408b82e9107e42b5f6b4c5e
David Cochran <david@alittlecode.com>
2013-10-21 15:07:44 -0500

    Update to latest framework basis point
    Updating JS, LESS, and fonts folders

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+$( document ).ready(function() {
 
+	// Set carousel options
+	$('.carousel').carousel({
+	  interval: 8000 // 8 seconds vs. default 5
+	});
+
+	// Add swipe to homepage carousel
+	$('#homepage-feature').hammer().on('swipeleft', function(){
+		$(this).carousel('next');
+	})
+	$('#homepage-feature').hammer().on('swiperight', function(){
+		$(this).carousel('prev');
+	})
+
+});
